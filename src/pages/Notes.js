92c59dcfc8cbe8edfd4524387c4d31c62bff1e04
MohdAlbadi <mohammed.albadi@swiftbeam.co>
2021-06-23 14:47:28 +0400

    Commite Card stoped

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,28 @@
+import { Grid , Paper ,Container} from '@material-ui/core'
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import NoteCard from '../components/NoteCard'
 
 export default function Notes() {
+  const [notes,SetNotes] =useState([])
+  useEffect(()=>
+  {
+  fetch('http://localhost:8220/notes')
+     .then(res => res.json())
+     .then(data => SetNotes(data))
+  },[])
   return (
-    <div>
-      Notes page
-    </div>
+   
+   <Container>
+      <Grid container>
+{notes.map(note =>(
+  <Grid item key={note.id} xs={12} md={6} lg={4}>
+    <Paper><NoteCard></NoteCard></Paper>  </Grid>
+    
+) )}
+</Grid>
+
+  </Container>  
   )
 }
